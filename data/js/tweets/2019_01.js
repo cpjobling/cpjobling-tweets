14de89d291204ca9c3c8c9934c30a2f3c1b456af
Chris P. Jobling <cpjobling@cpjobling.net>
2019-01-22 01:18:08 +0000

    Added 2 tweets

--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,333 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Sun Jan 20 21:33:58 +0000 2019",
+		"id": 1087100949309599700,
+		"id_str": "1087270639797440512",
+		"full_text": ".... Our winner is.... @KiuSum !!! Congratulations Kiu.... Stunning #mugafesto Engage and Empower 😍😍😍 #SocMedHE18 #SoCMedHE #LTHEchat #altc https://t.co/LnY1FdEHmA",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			139
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "mugafesto",
+					"indices": [
+						68,
+						78
+					]
+				},
+				{
+					"text": "SocMedHE18",
+					"indices": [
+						102,
+						113
+					]
+				},
+				{
+					"text": "SoCMedHE",
+					"indices": [
+						114,
+						123
+					]
+				},
+				{
+					"text": "LTHEchat",
+					"indices": [
+						124,
+						133
+					]
+				},
+				{
+					"text": "altc",
+					"indices": [
+						134,
+						139
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "KiuSum",
+					"name": "Kiu Sum",
+					"id": 246951711,
+					"id_str": "246951711",
+					"indices": [
+						23,
+						30
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1087100941852180500,
+					"id_str": "1087100941852180480",
+					"indices": [
+						140,
+						163
+					],
+					"media_url": "http://pbs.twimg.com/media/DxYojAIXgAAfYgj.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DxYojAIXgAAfYgj.jpg",
+					"url": "https://t.co/LnY1FdEHmA",
+					"display_url": "pic.twitter.com/LnY1FdEHmA",
+					"expanded_url": "https://twitter.com/SocMedHE/status/1087100949309599746/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 720,
+							"h": 722,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 722,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 678,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1087100941852180500,
+					"id_str": "1087100941852180480",
+					"indices": [
+						140,
+						163
+					],
+					"media_url": "http://pbs.twimg.com/media/DxYojAIXgAAfYgj.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DxYojAIXgAAfYgj.jpg",
+					"url": "https://t.co/LnY1FdEHmA",
+					"display_url": "pic.twitter.com/LnY1FdEHmA",
+					"expanded_url": "https://twitter.com/SocMedHE/status/1087100949309599746/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 720,
+							"h": 722,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 722,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 678,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3346395670,
+			"id_str": "3346395670",
+			"name": "@SocMedHE",
+			"screen_name": "SocMedHE",
+			"location": "Nottingham, England",
+			"description": "The Social Media for Learning in Higher Ed Event. Join us 9th January 2019 at @TrentUni #SocMedHE18 \nTweets by @RKChallen @horrocks_simon @suebecks",
+			"url": "https://t.co/jwYPKkAERp",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jwYPKkAERp",
+							"expanded_url": "https://socmedhe18.wordpress.com/",
+							"display_url": "socmedhe18.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1351,
+			"friends_count": 1580,
+			"listed_count": 66,
+			"created_at": "Fri Jun 26 08:20:41 +0000 2015",
+			"favourites_count": 2241,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1933,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "ABB8C2",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1059925392130236422/d3FujkpR_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1059925392130236422/d3FujkpR_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3346395670/1541575058",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 20,
+		"favorite_count": 42,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": ".... Our winner is.... @KiuSum !!! Congratulations Kiu.... Stunning #mugafesto Engage and Empower 😍😍😍 #SocMedHE18 #SoCMedHE #LTHEchat #altc https://t.co/LnY1FdEHmA"
+	},
+	{
+		"created_at": "Mon Jan 21 08:20:13 +0000 2019",
+		"id": 1087263583820546000,
+		"id_str": "1087263583820546048",
+		"full_text": "Read my Jan 21 'swan-uni-staff' Newsletter featuring “Top trans employers 2019” https://t.co/A9vxw0P03V",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			103
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/A9vxw0P03V",
+					"expanded_url": "https://nzzl.us/ZKQ9Amq",
+					"display_url": "nzzl.us/ZKQ9Amq",
+					"indices": [
+						80,
+						103
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://nuzzel.com/\" rel=\"nofollow\">Nuzzel</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4229341,
+			"id_str": "4229341",
+			"name": "Chris Jobling #FBPE",
+			"screen_name": "cpjobling",
+			"location": "Swansea",
+			"description": "Senior Lecturer, @SUEngineering, @SwanseaUni. Regular at #LTHEchat and #BYOD4L. Volunteer and Member of @theiet. Views expressed are my own, except re-tweets.",
+			"url": "https://t.co/vIMdJpfRKN",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vIMdJpfRKN",
+							"expanded_url": "http://about.me/cpjobling",
+							"display_url": "about.me/cpjobling",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1484,
+			"friends_count": 1196,
+			"listed_count": 128,
+			"created_at": "Wed Apr 11 19:13:11 +0000 2007",
+			"favourites_count": 10117,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17459,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1045593322012770304/sZ0LVya0_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1045593322012770304/sZ0LVya0_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4229341/1398287038",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Read my Jan 21 'swan-uni-staff' Newsletter featuring “Top trans employers 2019” https://t.co/A9vxw0P03V"
+	},
+	{
 		"created_at": "Sun Jan 20 08:21:12 +0000 2019",
 		"id": 1086901442705805300,
 		"id_str": "1086901442705805313",
