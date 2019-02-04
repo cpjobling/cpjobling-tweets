4aa5acac6b5110ddde5f4532197b1b15438f5235
Chris P. Jobling <cpjobling@cpjobling.net>
2019-02-04 01:18:04 +0000

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 17756,
-	"created_at": "Sun Feb 03 01:18:03 +0000 2019",
+	"tweets": 17759,
+	"created_at": "Mon Feb 04 01:18:03 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_02.js
+++ data/js/tweets/2019_02.js
@@ -1,6 +1,609 @@
 Grailbird.data.tweets_2019_02 = 
 [
 	{
+		"created_at": "Sun Feb 03 15:24:49 +0000 2019",
+		"id": 1092081480367521800,
+		"id_str": "1092081480367521793",
+		"full_text": "@Jermolene Thanks for sharing Jeremy",
+		"truncated": false,
+		"display_text_range": [
+			11,
+			36
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Jermolene",
+					"name": "Jeremy Ruston",
+					"id": 37113,
+					"id_str": "37113",
+					"indices": [
+						0,
+						10
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1091739937815359500,
+		"in_reply_to_status_id_str": "1091739937815359488",
+		"in_reply_to_user_id": 37113,
+		"in_reply_to_user_id_str": "37113",
+		"in_reply_to_screen_name": "Jermolene",
+		"user": {
+			"id": 4229341,
+			"id_str": "4229341",
+			"name": "Chris Jobling #FBPE",
+			"screen_name": "cpjobling",
+			"location": "Swansea",
+			"description": "Senior Lecturer, @SUEngineering, @SwanseaUni. Regular at #LTHEchat and #BYOD4L. Volunteer and Member of @theiet. Views expressed are my own, except re-tweets.",
+			"url": "https://t.co/vIMdJpfRKN",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vIMdJpfRKN",
+							"expanded_url": "http://about.me/cpjobling",
+							"display_url": "about.me/cpjobling",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1483,
+			"friends_count": 1195,
+			"listed_count": 127,
+			"created_at": "Wed Apr 11 19:13:11 +0000 2007",
+			"favourites_count": 10157,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17529,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1045593322012770304/sZ0LVya0_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1045593322012770304/sZ0LVya0_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4229341/1398287038",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@Jermolene Thanks for sharing Jeremy"
+	},
+	{
+		"created_at": "Sat Feb 02 16:47:39 +0000 2019",
+		"id": 1091739937815359500,
+		"id_str": "1092081320577167361",
+		"full_text": "Pretty jaw-dropping stuff https://t.co/ACZMniziLQ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			25
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/ACZMniziLQ",
+					"expanded_url": "https://twitter.com/UrbanFoxxxx/status/1091680575117770752",
+					"display_url": "twitter.com/UrbanFoxxxx/st…",
+					"indices": [
+						26,
+						49
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 37113,
+			"id_str": "37113",
+			"name": "Jeremy Ruston",
+			"screen_name": "Jermolene",
+			"location": "Oxford",
+			"description": "Learning to code since 1978. Creator of @TiddlyWiki. Hire me through @Federatial",
+			"url": "https://t.co/gwfAaOhbC3",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/gwfAaOhbC3",
+							"expanded_url": "http://jermolene.com/",
+							"display_url": "jermolene.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2136,
+			"friends_count": 829,
+			"listed_count": 113,
+			"created_at": "Sat Dec 02 15:45:37 +0000 2006",
+			"favourites_count": 1132,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4703,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFEEA8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/652498614355365888/6QIrG4G3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/652498614355365888/6QIrG4G3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/37113/1398195015",
+			"profile_link_color": "269132",
+			"profile_sidebar_border_color": "8A5228",
+			"profile_sidebar_fill_color": "D6B26B",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1091680575117770800,
+		"quoted_status_id_str": "1091680575117770752",
+		"quoted_status_permalink": {
+			"url": "https://t.co/ACZMniziLQ",
+			"expanded": "https://twitter.com/UrbanFoxxxx/status/1091680575117770752",
+			"display": "twitter.com/UrbanFoxxxx/st…"
+		},
+		"quoted_status": {
+			"created_at": "Sat Feb 02 12:51:46 +0000 2019",
+			"id": 1091680575117770800,
+			"id_str": "1091680575117770752",
+			"full_text": "A vast, searchable database of pre-1900 watercolours documenting the four corners of the world before the invention of photography - you can search by subject or location. Absolutely wonderful.\n\nhttps://t.co/gr81z2r6NK\n[pictured, the ruins at Luxor, ca. 1830] https://t.co/vqCzONc7Bb",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				259
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/gr81z2r6NK",
+						"expanded_url": "https://www.watercolourworld.org/",
+						"display_url": "watercolourworld.org",
+						"indices": [
+							195,
+							218
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1091679264917909500,
+						"id_str": "1091679264917909504",
+						"indices": [
+							260,
+							283
+						],
+						"media_url": "http://pbs.twimg.com/media/DyZsgeoXcAAAHzW.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DyZsgeoXcAAAHzW.jpg",
+						"url": "https://t.co/vqCzONc7Bb",
+						"display_url": "pic.twitter.com/vqCzONc7Bb",
+						"expanded_url": "https://twitter.com/UrbanFoxxxx/status/1091680575117770752/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1266,
+								"h": 908,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 488,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 861,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1091679264917909500,
+						"id_str": "1091679264917909504",
+						"indices": [
+							260,
+							283
+						],
+						"media_url": "http://pbs.twimg.com/media/DyZsgeoXcAAAHzW.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DyZsgeoXcAAAHzW.jpg",
+						"url": "https://t.co/vqCzONc7Bb",
+						"display_url": "pic.twitter.com/vqCzONc7Bb",
+						"expanded_url": "https://twitter.com/UrbanFoxxxx/status/1091680575117770752/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1266,
+								"h": 908,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 488,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 861,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 1091679306772820000,
+						"id_str": "1091679306772819968",
+						"indices": [
+							260,
+							283
+						],
+						"media_url": "http://pbs.twimg.com/media/DyZsi6jWsAAoI4l.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DyZsi6jWsAAoI4l.jpg",
+						"url": "https://t.co/vqCzONc7Bb",
+						"display_url": "pic.twitter.com/vqCzONc7Bb",
+						"expanded_url": "https://twitter.com/UrbanFoxxxx/status/1091680575117770752/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1192,
+								"h": 805,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 459,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1192,
+								"h": 805,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 1091679351450554400,
+						"id_str": "1091679351450554370",
+						"indices": [
+							260,
+							283
+						],
+						"media_url": "http://pbs.twimg.com/media/DyZslg_W0AIb53h.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DyZslg_W0AIb53h.jpg",
+						"url": "https://t.co/vqCzONc7Bb",
+						"display_url": "pic.twitter.com/vqCzONc7Bb",
+						"expanded_url": "https://twitter.com/UrbanFoxxxx/status/1091680575117770752/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 647,
+								"h": 879,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 501,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 647,
+								"h": 879,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 1091679409696849900,
+						"id_str": "1091679409696849920",
+						"indices": [
+							260,
+							283
+						],
+						"media_url": "http://pbs.twimg.com/media/DyZso5-WwAAUIDe.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DyZso5-WwAAUIDe.jpg",
+						"url": "https://t.co/vqCzONc7Bb",
+						"display_url": "pic.twitter.com/vqCzONc7Bb",
+						"expanded_url": "https://twitter.com/UrbanFoxxxx/status/1091680575117770752/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 447,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1064,
+								"h": 699,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1064,
+								"h": 699,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 3404085119,
+				"id_str": "3404085119",
+				"name": "Irène DB",
+				"screen_name": "UrbanFoxxxx",
+				"location": "London",
+				"description": "Architect turned accidental 'creative'. Flâneuse manquée. Disillusionist.",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 13445,
+				"friends_count": 370,
+				"listed_count": 332,
+				"created_at": "Wed Aug 05 11:53:52 +0000 2015",
+				"favourites_count": 11484,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 10442,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/760255331599544320/97KEPMB__normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/760255331599544320/97KEPMB__normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/3404085119/1449699668",
+				"profile_link_color": "A020F0",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2840,
+			"favorite_count": 5831,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 4,
+		"favorite_count": 14,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Pretty jaw-dropping stuff https://t.co/ACZMniziLQ"
+	},
+	{
+		"created_at": "Sun Feb 03 08:03:23 +0000 2019",
+		"id": 1091970391852179500,
+		"id_str": "1091970391852179457",
+		"full_text": "Read my Feb 3 'swan-uni-staff' Newsletter featuring “Making Connections” https://t.co/seQ0Bmkvom",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			96
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/seQ0Bmkvom",
+					"expanded_url": "https://nzzl.us/WrM27rA",
+					"display_url": "nzzl.us/WrM27rA",
+					"indices": [
+						73,
+						96
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://nuzzel.com/\" rel=\"nofollow\">Nuzzel</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4229341,
+			"id_str": "4229341",
+			"name": "Chris Jobling #FBPE",
+			"screen_name": "cpjobling",
+			"location": "Swansea",
+			"description": "Senior Lecturer, @SUEngineering, @SwanseaUni. Regular at #LTHEchat and #BYOD4L. Volunteer and Member of @theiet. Views expressed are my own, except re-tweets.",
+			"url": "https://t.co/vIMdJpfRKN",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vIMdJpfRKN",
+							"expanded_url": "http://about.me/cpjobling",
+							"display_url": "about.me/cpjobling",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1483,
+			"friends_count": 1195,
+			"listed_count": 127,
+			"created_at": "Wed Apr 11 19:13:11 +0000 2007",
+			"favourites_count": 10157,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17529,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1045593322012770304/sZ0LVya0_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1045593322012770304/sZ0LVya0_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4229341/1398287038",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Read my Feb 3 'swan-uni-staff' Newsletter featuring “Making Connections” https://t.co/seQ0Bmkvom"
+	},
+	{
 		"created_at": "Sat Feb 02 08:03:27 +0000 2019",
 		"id": 1091608020734349300,
 		"id_str": "1091608020734349312",
