efc1aff45c7cfe6efd9f613accf1d7549975bee6
Chris P. Jobling <cpjobling@cpjobling.net>
2019-07-29 02:18:05 +0100

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18565,
-	"created_at": "Sun Jul 28 01:18:04 +0000 2019",
+	"tweets": 18567,
+	"created_at": "Mon Jul 29 01:18:04 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_07.js
+++ data/js/tweets/2019_07.js
@@ -1,6 +1,207 @@
 Grailbird.data.tweets_2019_07 = 
 [
 	{
+		"created_at": "Sun Jul 28 09:05:00 +0000 2019",
+		"id": 1155096176460476400,
+		"id_str": "1155403774137196544",
+		"full_text": "Rules for my staff:\nLet’s always be in open dialogue\nLet’s draw on shared values, analysis &amp; evidence\nLet’s work for systemic change to address inequities\nLet’s encourage creativity &amp; flexibility\nLet’s remember we’re all in this together\n\nAnd let’s put commas wherever we see fit",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			287
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 89804497,
+			"id_str": "89804497",
+			"name": "Prof Dilly Fung",
+			"screen_name": "DevonDilly",
+			"location": "",
+			"description": "Pro-Director for Education at LSE, London UK. Committed to higher education, and to connecting education and research for a better world #PFHEA #FRSA My tweets",
+			"url": "https://t.co/SQZ9HJDgiS",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/SQZ9HJDgiS",
+							"expanded_url": "http://discovery.ucl.ac.uk/1558776/1/A-Connected-Curriculum-for-Higher-Education.pdf",
+							"display_url": "discovery.ucl.ac.uk/1558776/1/A-Co…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3159,
+			"friends_count": 1997,
+			"listed_count": 53,
+			"created_at": "Fri Nov 13 22:03:25 +0000 2009",
+			"favourites_count": 21318,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 6758,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/999946987830202368/S3DTPmqY_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/999946987830202368/S3DTPmqY_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/89804497/1407693701",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 24,
+		"favorite_count": 125,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "Rules for my staff:\nLet’s always be in open dialogue\nLet’s draw on shared values, analysis &amp; evidence\nLet’s work for systemic change to address inequities\nLet’s encourage creativity &amp; flexibility\nLet’s remember we’re all in this together\n\nAnd let’s put commas wherever we see fit"
+	},
+	{
+		"created_at": "Sun Jul 28 07:03:20 +0000 2019",
+		"id": 1155373155088777200,
+		"id_str": "1155373155088777216",
+		"full_text": "Read the Jul 28 Newsletter curated by my PLN featuring “ITV News exclusive: Jacob Rees-Mogg issues style guide…” https://t.co/aqnOVLD9od",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			136
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/aqnOVLD9od",
+					"expanded_url": "https://nzzl.us/v2iucDd",
+					"display_url": "nzzl.us/v2iucDd",
+					"indices": [
+						113,
+						136
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://nuzzel.com/\" rel=\"nofollow\">Nuzzel</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4229341,
+			"id_str": "4229341",
+			"name": "Chris Jobling",
+			"screen_name": "cpjobling",
+			"location": "Swansea",
+			"description": "Senior Lecturer, @SUEngineering, @SwanseaUni. Regular at #LTHEchat and #BYOD4L. Volunteer and Member of @theiet. Not on Facebook!",
+			"url": "https://t.co/vIMdJpfRKN",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vIMdJpfRKN",
+							"expanded_url": "http://about.me/cpjobling",
+							"display_url": "about.me/cpjobling",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1525,
+			"friends_count": 1196,
+			"listed_count": 125,
+			"created_at": "Wed Apr 11 19:13:11 +0000 2007",
+			"favourites_count": 10474,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18462,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1045593322012770304/sZ0LVya0_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1045593322012770304/sZ0LVya0_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4229341/1398287038",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Read the Jul 28 Newsletter curated by my PLN featuring “ITV News exclusive: Jacob Rees-Mogg issues style guide…” https://t.co/aqnOVLD9od"
+	},
+	{
 		"created_at": "Sat Jul 27 07:03:58 +0000 2019",
 		"id": 1155010926254403600,
 		"id_str": "1155010926254403584",
