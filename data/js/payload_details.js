d690b59e2a063a3bdafed59da6b803e70f11552e
Chris P. Jobling <cpjobling@cpjobling.net>
2020-08-05 02:18:06 +0100

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 20269,
-	"created_at": "Tue Aug 04 01:18:05 +0000 2020",
+	"tweets": 20272,
+	"created_at": "Wed Aug 05 01:18:05 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_08.js
+++ data/js/tweets/2020_08.js
@@ -1,6 +1,402 @@
 Grailbird.data.tweets_2020_08 = 
 [
 	{
+		"created_at": "Tue Aug 04 12:07:00 +0000 2020",
+		"id": 1289342626878169000,
+		"id_str": "1290620248874459136",
+		"full_text": "Learning from each other to continue to improve what we do. A space to make your teaching resources &amp; pedagogical research discoverable, shareable and citable. Please visit: https://t.co/1A090uqziI to read our welcome guide. https://t.co/Z1n8wFn3A0",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			228
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/1A090uqziI",
+					"expanded_url": "https://figshare.edgehill.ac.uk/articles/presentation/NTR_-_Welcome_pdf/12673016",
+					"display_url": "figshare.edgehill.ac.uk/articles/prese…",
+					"indices": [
+						178,
+						201
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1289341573277327400,
+					"id_str": "1289341573277327361",
+					"indices": [
+						229,
+						252
+					],
+					"media_url": "http://pbs.twimg.com/media/EeSpVGVWoAE9EhM.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EeSpVGVWoAE9EhM.jpg",
+					"url": "https://t.co/Z1n8wFn3A0",
+					"display_url": "pic.twitter.com/Z1n8wFn3A0",
+					"expanded_url": "https://twitter.com/NTRepository/status/1289342626878169088/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 656,
+							"h": 382,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 656,
+							"h": 382,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 656,
+							"h": 382,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1289341573277327400,
+					"id_str": "1289341573277327361",
+					"indices": [
+						229,
+						252
+					],
+					"media_url": "http://pbs.twimg.com/media/EeSpVGVWoAE9EhM.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EeSpVGVWoAE9EhM.jpg",
+					"url": "https://t.co/Z1n8wFn3A0",
+					"display_url": "pic.twitter.com/Z1n8wFn3A0",
+					"expanded_url": "https://twitter.com/NTRepository/status/1289342626878169088/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 656,
+							"h": 382,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 656,
+							"h": 382,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 656,
+							"h": 382,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1269949194292297700,
+			"id_str": "1269949194292297728",
+			"name": "The National Teaching Repository",
+			"screen_name": "NTRepository",
+			"location": "",
+			"description": "Learning from each other to continue to improve what we do. A space to make your teaching resources & pedagogical research discoverable, shareable and citable.",
+			"url": "https://t.co/PH1wrQNGEe",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PH1wrQNGEe",
+							"expanded_url": "https://figshare.edgehill.ac.uk/The_National_Teaching_Repository",
+							"display_url": "figshare.edgehill.ac.uk/The_National_T…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 56,
+			"friends_count": 0,
+			"listed_count": 0,
+			"created_at": "Mon Jun 08 11:07:46 +0000 2020",
+			"favourites_count": 1,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 2,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1270497708810395649/wf2v3Utn_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1270497708810395649/wf2v3Utn_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1269949194292297728/1591745244",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 12,
+		"favorite_count": 31,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Learning from each other to continue to improve what we do. A space to make your teaching resources &amp; pedagogical research discoverable, shareable and citable. Please visit: https://t.co/1A090uqziI to read our welcome guide. https://t.co/Z1n8wFn3A0"
+	},
+	{
+		"created_at": "Tue Aug 04 11:05:55 +0000 2020",
+		"id": 1290604875215261700,
+		"id_str": "1290604875215261697",
+		"full_text": "Haus der Bayerischen Geschichte - Museum on Google Maps - avoid until COVID-19 ends. https://t.co/3M26yjRF3Y",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			108
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/3M26yjRF3Y",
+					"expanded_url": "https://goo.gl/maps/EzWhgYWeaFyaP1Hy7",
+					"display_url": "goo.gl/maps/EzWhgYWea…",
+					"indices": [
+						85,
+						108
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4229341,
+			"id_str": "4229341",
+			"name": "Chris Jobling",
+			"screen_name": "cpjobling",
+			"location": "Swansea",
+			"description": "Senior Lecturer, @SUEngineering, @SwanseaUni. Regular at #LTHEchat and #BYOD4L. Volunteer and Member of @theiet. Not on Facebook!",
+			"url": "https://t.co/vIMdJpfRKN",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vIMdJpfRKN",
+							"expanded_url": "http://about.me/cpjobling",
+							"display_url": "about.me/cpjobling",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1579,
+			"friends_count": 1211,
+			"listed_count": 122,
+			"created_at": "Wed Apr 11 19:13:11 +0000 2007",
+			"favourites_count": 11223,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 20478,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1270415818589974529/fvhDHM3d_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1270415818589974529/fvhDHM3d_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4229341/1398287038",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "de",
+		"text": "Haus der Bayerischen Geschichte - Museum on Google Maps - avoid until COVID-19 ends. https://t.co/3M26yjRF3Y"
+	},
+	{
+		"created_at": "Tue Aug 04 07:05:02 +0000 2020",
+		"id": 1290544255258300400,
+		"id_str": "1290544255258300422",
+		"full_text": "Read the Aug 4 Newsletter curated by my PLN featuring “How the Pandemic Defeated America” https://t.co/InYFYnByTp",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			113
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/InYFYnByTp",
+					"expanded_url": "https://nzzl.us/T8tEIFa",
+					"display_url": "nzzl.us/T8tEIFa",
+					"indices": [
+						90,
+						113
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://nuzzel.com/\" rel=\"nofollow\">Nuzzel</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4229341,
+			"id_str": "4229341",
+			"name": "Chris Jobling",
+			"screen_name": "cpjobling",
+			"location": "Swansea",
+			"description": "Senior Lecturer, @SUEngineering, @SwanseaUni. Regular at #LTHEchat and #BYOD4L. Volunteer and Member of @theiet. Not on Facebook!",
+			"url": "https://t.co/vIMdJpfRKN",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vIMdJpfRKN",
+							"expanded_url": "http://about.me/cpjobling",
+							"display_url": "about.me/cpjobling",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1579,
+			"friends_count": 1211,
+			"listed_count": 122,
+			"created_at": "Wed Apr 11 19:13:11 +0000 2007",
+			"favourites_count": 11223,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 20478,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1270415818589974529/fvhDHM3d_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1270415818589974529/fvhDHM3d_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4229341/1398287038",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Read the Aug 4 Newsletter curated by my PLN featuring “How the Pandemic Defeated America” https://t.co/InYFYnByTp"
+	},
+	{
 		"created_at": "Mon Aug 03 07:04:27 +0000 2020",
 		"id": 1290181722747936800,
 		"id_str": "1290181722747936768",
