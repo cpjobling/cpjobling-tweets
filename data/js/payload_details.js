bea7d18afdb245009b445e90817ac8490a111e12
Chris P. Jobling <cpjobling@cpjobling.net>
2020-09-20 02:18:07 +0100

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 20552,
-	"created_at": "Sat Sep 19 01:18:07 +0000 2020",
+	"tweets": 20554,
+	"created_at": "Sun Sep 20 01:18:05 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_09.js
+++ data/js/tweets/2020_09.js
@@ -1,6 +1,215 @@
 Grailbird.data.tweets_2020_09 = 
 [
 	{
+		"created_at": "Sat Sep 19 17:17:26 +0000 2020",
+		"id": 1307276763152756700,
+		"id_str": "1307368214867910656",
+		"full_text": "Building Simple Games in #MicrosoftForms [thread]\n\nGames can be a great way to engage students, and there are loads of ways to play games in a face to face session. However, with the shift to online/blended learning, we need some new approaches. This is one in MSForms [1/11]",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			275
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "MicrosoftForms",
+					"indices": [
+						25,
+						40
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 405910853,
+			"id_str": "405910853",
+			"name": "Dr Chris Headleand",
+			"screen_name": "ChrisHeadleand",
+			"location": "Lincoln, England",
+			"description": "Computer Scientist and Director of Teaching and Learning; VR Researcher; Interested in learning communities; Student Engagement evangelist! #SFHEA Dad",
+			"url": "https://t.co/HA0WAkZJl7",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/HA0WAkZJl7",
+							"expanded_url": "https://chrisheadleand.com/",
+							"display_url": "chrisheadleand.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3694,
+			"friends_count": 1178,
+			"listed_count": 62,
+			"created_at": "Sun Nov 06 00:10:12 +0000 2011",
+			"favourites_count": 16665,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 16660,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1295457286828306437/7hUsCRav_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1295457286828306437/7hUsCRav_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/405910853/1445964281",
+			"profile_link_color": "445F74",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 22,
+		"favorite_count": 67,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "Building Simple Games in #MicrosoftForms [thread]\n\nGames can be a great way to engage students, and there are loads of ways to play games in a face to face session. However, with the shift to online/blended learning, we need some new approaches. This is one in MSForms [1/11]"
+	},
+	{
+		"created_at": "Sat Sep 19 07:05:02 +0000 2020",
+		"id": 1307214096341840000,
+		"id_str": "1307214096341839874",
+		"full_text": "Read the Sep 19 Newsletter curated by my PLN featuring “Justice Ruth Bader Ginsburg, Champion Of Gender…” https://t.co/fuYI85M9Jx",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			129
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/fuYI85M9Jx",
+					"expanded_url": "https://nzzl.us/braDSAr",
+					"display_url": "nzzl.us/braDSAr",
+					"indices": [
+						106,
+						129
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://nuzzel.com/\" rel=\"nofollow\">Nuzzel</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4229341,
+			"id_str": "4229341",
+			"name": "Chris Jobling",
+			"screen_name": "cpjobling",
+			"location": "Swansea",
+			"description": "Senior Lecturer, @SUEngineering, @SwanseaUni. Regular at #LTHEchat and #BYOD4L. Volunteer and Member of @theiet. Not on Facebook!",
+			"url": "https://t.co/vIMdJpfRKN",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vIMdJpfRKN",
+							"expanded_url": "http://about.me/cpjobling",
+							"display_url": "about.me/cpjobling",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1586,
+			"friends_count": 1217,
+			"listed_count": 122,
+			"created_at": "Wed Apr 11 19:13:11 +0000 2007",
+			"favourites_count": 11489,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 20805,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1270415818589974529/fvhDHM3d_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1270415818589974529/fvhDHM3d_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4229341/1398287038",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Read the Sep 19 Newsletter curated by my PLN featuring “Justice Ruth Bader Ginsburg, Champion Of Gender…” https://t.co/fuYI85M9Jx"
+	},
+	{
 		"created_at": "Fri Sep 18 12:43:18 +0000 2020",
 		"id": 1306914585832837000,
 		"id_str": "1306936835894128640",
