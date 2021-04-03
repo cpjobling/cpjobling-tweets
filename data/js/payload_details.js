887ac6abfcded8a294110d0978c32b9756557ae7
Chris P. Jobling <cpjobling@cpjobling.net>
2021-04-03 02:18:05 +0100

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 21423,
-	"created_at": "Sat Mar 27 01:18:05 +0000 2021",
+	"tweets": 21425,
+	"created_at": "Sat Apr 03 01:18:05 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_04.js
@@ -0,0 +1,386 @@
+Grailbird.data.tweets_2021_04 = 
+[
+	{
+		"created_at": "Fri Apr 02 07:07:00 +0000 2021",
+		"id": 1377654065887207400,
+		"id_str": "1377880224831012866",
+		"full_text": "Let's get behind the learning analytics and do better: “Because what we measure sends a message about what matters.” ~ @ammienoot\n\"We need to expand our data literacy... [in #highered]... we assume that everybody knows these things, but we don't.\" @dgasevic cc: @YearsEd https://t.co/GNCITeDUao",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			270
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "highered",
+					"indices": [
+						174,
+						183
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ammienoot",
+					"name": "Anne-Marie Scott",
+					"id": 22673907,
+					"id_str": "22673907",
+					"indices": [
+						119,
+						129
+					]
+				},
+				{
+					"screen_name": "dgasevic",
+					"name": "Dragan Gasevic",
+					"id": 69653552,
+					"id_str": "69653552",
+					"indices": [
+						248,
+						257
+					]
+				},
+				{
+					"screen_name": "YearsEd",
+					"name": "25 Years Of Ed Tech: The Audio Version",
+					"id": 1312472054130602000,
+					"id_str": "1312472054130601984",
+					"indices": [
+						262,
+						270
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/GNCITeDUao",
+					"expanded_url": "https://twitter.com/YearsEd/status/1377531469560750081",
+					"display_url": "twitter.com/YearsEd/status…",
+					"indices": [
+						271,
+						294
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 16708242,
+			"id_str": "16708242",
+			"name": "Laura Pasquini, Ph.D.",
+			"screen_name": "laurapasquini",
+			"location": "🇨🇦 + 🇨🇮 + 🇺🇸 = home",
+			"description": "Learning & Performance #DigitalLearning #LXD #Coaching #OER #PLN #AcAdv mentoring #FemEdTech; Pods🎙️@YearsEd @CoachingThruIt & @InVinoFab https://t.co/OCjE7dGBIy",
+			"url": "https://t.co/hH6MLGMIcn",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/hH6MLGMIcn",
+							"expanded_url": "https://linktr.ee/techknowtools",
+							"display_url": "linktr.ee/techknowtools",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/OCjE7dGBIy",
+							"expanded_url": "http://pronoun.is/she",
+							"display_url": "pronoun.is/she",
+							"indices": [
+								138,
+								161
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 10408,
+			"friends_count": 7157,
+			"listed_count": 1013,
+			"created_at": "Sun Oct 12 14:51:58 +0000 2008",
+			"favourites_count": 18573,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 67283,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "A7D8F2",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme10/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme10/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1355463932148805633/1vPXgTKY_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1355463932148805633/1vPXgTKY_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/16708242/1612636416",
+			"profile_link_color": "94D497",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "7AC3EE",
+			"profile_text_color": "1BB884",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1377531469560750000,
+		"quoted_status_id_str": "1377531469560750081",
+		"quoted_status_permalink": {
+			"url": "https://t.co/GNCITeDUao",
+			"expanded": "https://twitter.com/YearsEd/status/1377531469560750081",
+			"display": "twitter.com/YearsEd/status…"
+		},
+		"quoted_status": {
+			"created_at": "Thu Apr 01 08:01:10 +0000 2021",
+			"id": 1377531469560750000,
+			"id_str": "1377531469560750081",
+			"full_text": "★ Just published a new episode of 25 Years of Ed Tech: Between the Chapters #21 analyzing the metrics &amp; data in learning with @ammienoot &amp; @dgasevic. Listen: https://t.co/hjdl8iMaKw",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				189
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "ammienoot",
+						"name": "Anne-Marie Scott",
+						"id": 22673907,
+						"id_str": "22673907",
+						"indices": [
+							130,
+							140
+						]
+					},
+					{
+						"screen_name": "dgasevic",
+						"name": "Dragan Gasevic",
+						"id": 69653552,
+						"id_str": "69653552",
+						"indices": [
+							147,
+							156
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/hjdl8iMaKw",
+						"expanded_url": "https://share.transistor.fm/s/76f686d0",
+						"display_url": "share.transistor.fm/s/76f686d0",
+						"indices": [
+							166,
+							189
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://transistor.fm\" rel=\"nofollow\">Transistor.fm</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1312472054130602000,
+				"id_str": "1312472054130601984",
+				"name": "25 Years Of Ed Tech: The Audio Version",
+				"screen_name": "YearsEd",
+				"location": "",
+				"description": "Twitter account for the serialized audio version of Martin Weller's 25 Years of Ed Tech book & bonus podcast Between the Chapters Tweets by @edtechfactotum",
+				"url": "https://t.co/BPiRsGtHe9",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/BPiRsGtHe9",
+								"expanded_url": "http://25years.opened.ca",
+								"display_url": "25years.opened.ca",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 180,
+				"friends_count": 0,
+				"listed_count": 4,
+				"created_at": "Sat Oct 03 19:18:25 +0000 2020",
+				"favourites_count": 118,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 119,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1312472262159687683/y_t45Ol-_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1312472262159687683/y_t45Ol-_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1312472054130601984/1601752908",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 3,
+			"favorite_count": 6,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 3,
+		"favorite_count": 15,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Let's get behind the learning analytics and do better: “Because what we measure sends a message about what matters.” ~ @ammienoot\n\"We need to expand our data literacy... [in #highered]... we assume that everybody knows these things, but we don't.\" @dgasevic cc: @YearsEd https://t.co/GNCITeDUao"
+	},
+	{
+		"created_at": "Fri Apr 02 07:05:26 +0000 2021",
+		"id": 1377551152011735000,
+		"id_str": "1377879834542624770",
+		"full_text": "I mean, what twisted genius went with the idea of pretending that the Chair is a former MP and friend of the Prime Minister with no experience in the education sector?\n\nSuperb.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			176
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1377550800134738000,
+		"in_reply_to_status_id_str": "1377550800134737922",
+		"in_reply_to_user_id": 12219232,
+		"in_reply_to_user_id_str": "12219232",
+		"in_reply_to_screen_name": "dkernohan",
+		"user": {
+			"id": 12219232,
+			"id_str": "12219232",
+			"name": "David Kernohan",
+			"screen_name": "dkernohan",
+			"location": "Dursley, England",
+			"description": "Associate Editor/ Data Person @Wonkhe.",
+			"url": "https://t.co/t4yXduFlpo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/t4yXduFlpo",
+							"expanded_url": "http://followersoftheapocalyp.se",
+							"display_url": "followersoftheapocalyp.se",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7055,
+			"friends_count": 2376,
+			"listed_count": 331,
+			"created_at": "Mon Jan 14 15:05:54 +0000 2008",
+			"favourites_count": 10194,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 64787,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme3/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme3/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1216363550148571142/MK3ZB5h-_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1216363550148571142/MK3ZB5h-_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/12219232/1554198155",
+			"profile_link_color": "7FDBB6",
+			"profile_sidebar_border_color": "B5B5B5",
+			"profile_sidebar_fill_color": "B8B8B8",
+			"profile_text_color": "5C5C5C",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 20,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "I mean, what twisted genius went with the idea of pretending that the Chair is a former MP and friend of the Prime Minister with no experience in the education sector?\n\nSuperb."
+	}
+]
