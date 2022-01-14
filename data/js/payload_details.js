28780d94c862f3376c1f9330233dbba0366002c5
Chris P. Jobling <cpjobling@cpjobling.net>
2022-01-14 01:18:04 +0000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 21683,
-	"created_at": "Fri Oct 22 01:18:05 +0000 2021",
+	"tweets": 21684,
+	"created_at": "Fri Jan 14 01:18:03 +0000 2022",
 	"lang": "en"
 }
---
+++ data/js/tweets/2022_01.js
@@ -0,0 +1,118 @@
+Grailbird.data.tweets_2022_01 = 
+[
+	{
+		"created_at": "Thu Jan 13 08:14:31 +0000 2022",
+		"id": 1481368740679884800,
+		"id_str": "1481540145975369730",
+		"full_text": "A6 I have said this before, but without the, literally exceptional @KiuSum , where is the student voice in #LTHEchat ? And therein lies my problem. We simply don't have a culture of inclusive discussions that would underpin the mindful learning transition.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			256
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "LTHEchat",
+					"indices": [
+						107,
+						116
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "KiuSum",
+					"name": "Kiu Sum",
+					"id": 246951711,
+					"id_str": "246951711",
+					"indices": [
+						67,
+						74
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 240186025,
+			"id_str": "240186025",
+			"name": "Simon Lancaster",
+			"screen_name": "S_J_Lancaster",
+			"location": "Norwich or not",
+			"description": "He / him. Exiled European, Professor of Chemistry Education, National Teaching Fellow.",
+			"url": "https://t.co/bvNjUECd3O",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/bvNjUECd3O",
+							"expanded_url": "http://www.uea.ac.uk/che/people/faculty/lancasters",
+							"display_url": "uea.ac.uk/che/people/fac…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8265,
+			"friends_count": 6852,
+			"listed_count": 240,
+			"created_at": "Wed Jan 19 10:32:36 +0000 2011",
+			"favourites_count": 10916,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 53795,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "642D8B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme10/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme10/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1441074913683513364/q-GFm9aj_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1441074913683513364/q-GFm9aj_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/240186025/1372342264",
+			"profile_link_color": "FF0000",
+			"profile_sidebar_border_color": "65B0DA",
+			"profile_sidebar_fill_color": "7AC3EE",
+			"profile_text_color": "3D1957",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 9,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "A6 I have said this before, but without the, literally exceptional @KiuSum , where is the student voice in #LTHEchat ? And therein lies my problem. We simply don't have a culture of inclusive discussions that would underpin the mindful learning transition."
+	}
+]
